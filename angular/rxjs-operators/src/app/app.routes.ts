1388b469fa6fbcc0061c1f52e8b83f9db921385f
Kamil Soloducha <kamilsoloducha@gmail.com>
2024-08-25 16:25:43 +0200

    merge map explanation

--- angular/rxjs-operators/src/app/app.routes.ts
+++ angular/rxjs-operators/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { SwitchMapComponent } from './pages/switch-map/switch-map.component';
+import { MergeMapComponent } from './pages/merge-map/merge-map.component';
 
 export const routes: Routes = [
   { path: 'switch-map', component: SwitchMapComponent },
+  { path: 'merge-map', component: MergeMapComponent },
 ];
---
+++ angular/rxjs-operators/src/app/pages/merge-map/merge-map.component.ts
@@ -0,0 +1,55 @@
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  signal,
+  ViewChild,
+} from '@angular/core';
+import { fromEvent, map, mergeMap } from 'rxjs';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './merge-map.component.html',
+})
+export class MergeMapComponent implements AfterViewInit {
+  @ViewChild('brokenInput1') brokenInput1?: ElementRef;
+  @ViewChild('brokenInput2') brokenInput2?: ElementRef;
+  brokenResult = signal('');
+
+  @ViewChild('workingInput1') workingInput1?: ElementRef;
+  @ViewChild('workingInput2') workingInput2?: ElementRef;
+  workingResult = signal('');
+
+  ngAfterViewInit(): void {
+    // broken - it's updating result after each input changed, but it's ignoring other value
+    fromEvent(this.brokenInput1?.nativeElement, 'input').subscribe(
+      (value: any) => this.brokenResult.set(value.target.value as any)
+    );
+    fromEvent(this.brokenInput2?.nativeElement, 'input').subscribe(
+      (value: any) => this.brokenResult.set(value.target.value as any)
+    );
+
+    // working - it's taking into account both values
+    const ob1$ = fromEvent<InputEvent>(
+      this.workingInput1?.nativeElement,
+      'input'
+    );
+    const ob2$ = fromEvent<InputEvent>(
+      this.workingInput2?.nativeElement,
+      'input'
+    );
+
+    ob1$
+      .pipe(
+        mergeMap((event1: any) => {
+          return ob2$.pipe(
+            map((event2: any) => {
+              return event1.target.value + ' ' + event2.target.value;
+            })
+          );
+        })
+      )
+      .subscribe((mergedValue) => this.workingResult.set(mergedValue));
+  }
+}
